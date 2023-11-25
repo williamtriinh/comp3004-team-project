d98af8defc66f6226338a81849adf180dd74e8dd
William Trinh <40365041+williamtriinh@users.noreply.github.com>
2023-11-24 20:48:27 -0500

    Add flashing light to the current step (only for check responsiveness) (#4)
    * Setup state
    
    * Rename AEDStateInterface to BaseState
    
    * Add comments to the BaseState
    
    * Add self test state
    
    * Add console logging
    
    * Add CheckResponsivenessState
    
    * Add CallForHelpState
    
    * Add AttachDefibrillatorPadsState
    
    * Add analyzing state
    
    * Add perform cpr state
    
    * Add basic state transition
    
    * Initial states implementation (#3)
    
    * Add electrodes installation simulation widget
    
    * Add batteries widget
    
    * Refactor state classes
    
    * Fix the states
    
    * Update BaseState's togglePower() method
    
    * Update comments
    
    * Update status indicator after self-tests
    
    * Add 'on' images
    
    * Update application.qrc
    
    * Add flashing to the AEDImage instances
    
    * Update comments
    
    * Update green ring to not use AEDImage

--- aed-simulator/src/aedimage.cpp
+++ aed-simulator/src/aedimage.cpp
@@ -1,14 +1,43 @@
 #include "aedimage.h"
 
-#include <QPixmap>
+#include "mainwindow.h"
+
 #include <QSizePolicy>
 
-AEDImage::AEDImage(QString imagePath, int height, QWidget *parent)
+AEDImage::AEDImage(MainWindow *mainWindow, QString imageName, int height, QString stateName, QWidget *parent)
     : QLabel(parent)
 {
-    QPixmap pixmap = QPixmap(imagePath).scaledToHeight(height);
-    setPixmap(pixmap);
+    this->mainWindow = mainWindow;
+    connect(mainWindow, &MainWindow::stateChanged, this, &AEDImage::handleStateChanged);
 
-    // Set the widget's size to the size of the pixmap
-    setFixedSize(pixmap.size());
+    timer = new QTimer(this);
+    timer->setSingleShot(true);
+    connect(timer, &QTimer::timeout, this, &AEDImage::toggleLight);
+
+    illuminated = false;
+    this->stateName = stateName;
+
+    offPixmap = QPixmap(":/images/steps/" + imageName + "_off.png").scaledToHeight(height);
+    onPixmap = QPixmap(":/images/steps/" + imageName + "_on.png").scaledToHeight(height);
+    setPixmap(offPixmap);
+    setFixedSize(offPixmap.size()); // Set the widget's size to the size of the pixmap
+}
+
+void AEDImage::toggleLight()
+{
+    illuminated = !illuminated;
+    setPixmap(illuminated ? onPixmap : offPixmap);
+    timer->start(illuminated ? AEDImage::LIGHT_ON_DURATION_MS : AEDImage::LIGHT_OFF_DURATION_MS);
+}
+
+void AEDImage::handleStateChanged(BaseState *state)
+{
+    if (stateName == state->getStateName())
+    {
+        toggleLight();
+    } else {
+        timer->stop();
+        illuminated = false;
+        setPixmap(offPixmap);
+    }
 }
