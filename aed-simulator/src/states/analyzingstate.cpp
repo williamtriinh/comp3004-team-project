a6314da4df525dd66ee2350a8198fca0a990c98c
student <eqmoore1@gmail.com>
2023-11-25 19:35:43 -0500

    Changed the analyzingstate class to make sure user has selected a patients status

--- aed-simulator/src/states/analyzingstate.cpp
+++ aed-simulator/src/states/analyzingstate.cpp
@@ -24,21 +24,41 @@ void AnalyzingState::initialize(){
 
 void AnalyzingState::execute()
 {
-    if(context->getPatientStatus() == MainWindow::PatientStatus::VHAB){
+    bool shockAdvised = true;
+    while(context->getPatientStatus() == MainWindow::PatientStatus::DEFAULT){
+        if(context->getPatientStatus() == MainWindow::PatientStatus::VHAB){
 
-        context->displayVTACHECG();
+            context->displayVTACHECG();
 
+        }
+        else if(context->getPatientStatus() == MainWindow::PatientStatus::VTACH){
+            context->displayVHABECG();
+
+        }
+        else if(context->getPatientStatus() == MainWindow::PatientStatus::NORMAL){
+            context->displayNormalECG();
+            shockAdvised = false;
+
+        }
+        else{
+            qDebug() << "Select Patient's Status";
+            timer->start(3000);
+        }
     }
-    else if(context->getPatientStatus() == MainWindow::PatientStatus::VTACH){
-        context->displayVHABECG();
-
-    }
-    else if(context->getPatientStatus() == MainWindow::PatientStatus::NORMAL){
-        context->displayNormalECG();
-
-    }
-    else{
-        qDebug() << "Select Patient's Status";
+    if(shockAdvised){
+        if(context->getBattery()>10){
+            context->playMessage("Give STAND CLEAR Warning. SO NOT touch patient");
+            timer->start(3000);
+            context->playMessage("Shock will be delivered in three, two, one ....");
+            timer->start(1000);
+            context->playMessage("Shock delivered");
+            timer->start(1000);
+            context->setBattery(context->getBattery()-10);
+        }
+        else{
+            context->playMessage("Not enough battery");
+            timer->start(1000);
+        }
     }
 
 
