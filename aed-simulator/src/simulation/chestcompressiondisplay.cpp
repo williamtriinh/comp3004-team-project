d2ef00b9c859641d1ec06839aa794c5e692fc857
williamtriinh <williamtrinh2039@gmail.com>
2023-12-01 15:47:27 -0500

    Add chest compression meter images

---
+++ aed-simulator/src/simulation/chestcompressiondisplay.cpp
@@ -0,0 +1,6 @@
+#include "chestcompressiondisplay.h"
+
+ChestCompressionDisplay::ChestCompressionDisplay(QWidget *parent) : QLabel(parent)
+{
+
+}
---
+++ aed-simulator/src/simulation/chestcompressiondisplay.h
@@ -0,0 +1,12 @@
+#ifndef CHESTCOMPRESSIONDISPLAY_H
+#define CHESTCOMPRESSIONDISPLAY_H
+
+#include <QLabel>
+
+class ChestCompressionDisplay : public QLabel
+{
+public:
+    ChestCompressionDisplay(QWidget *parent = nullptr);
+};
+
+#endif // CHESTCOMPRESSIONDISPLAY_H
